bea7490b5b5e75474ec9679256ef833483afd799
Charlie Brennan <charliebrennan50@gmail.com>
2020-09-04 07:18:32 -0400

    Added Map Capability

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,11 @@
+var mymap = L.map('mapid').setView([0,0], 8);
 
 document.getElementById('myBtn').addEventListener("click", function () {
   if ('geolocation' in navigator) {
     console.log("available");
     navigator.geolocation.getCurrentPosition(async position => {
-      let lat = position.coords.latitude;
-      let long = position.coords.longitude;
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
       document.getElementById('latitude').textContent = lat.toFixed(4);
       document.getElementById('longitude').textContent = long.toFixed(4);
 
@@ -41,8 +42,27 @@ document.getElementById('myBtn').addEventListener("click", function () {
       fetch('/api', options).then(response => {
         console.log(response);
       });
+
+      updateMap(lat, long);
+
     });
   } else {
     console.log("not available");
   }
 });
+
+function updateMap(lat,long) {
+
+  //Code to setup map tiles useing mapbox
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiY2hhcmxpZWJyZW5uYW41MCIsImEiOiJja2VnejBjMWswbGoyMzBxcTFhYW00ZmkwIn0.UN50EPaU0pu2JV131L422g'
+  }).addTo(mymap);
+  mymap.setView(new L.LatLng(lat, long), 8);
+  let popupText = `Latutude: ${lat.toFixed(4)}, Longitude: ${long.toFixed(4)}`
+  var marker = L.marker([lat, long]).addTo(mymap).bindPopup(popupText);
+}
